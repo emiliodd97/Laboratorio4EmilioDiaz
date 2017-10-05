e2da97923f2ba631c4801cbef035ba81bc022f3c
emiliodd97 <dia15316@uvg.edu.gt>
2017-10-05 16:31:40 -0600

    Version Final

--- espiralEmilioDiaz.cpp
+++ espiralEmilioDiaz.cpp
@@ -8,22 +8,19 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <iostream>
+// Definiciones para la matriz del espiral
 #define UP 1
 #define DOWN 2
 #define LEFT 3
 #define RIGHT 4
 #define MAXSZ 400
 #define NUMTHREADS 8
-/*
-*/
 using namespace cv;
-//Imagen a ser modificada
 Mat espiral(MAXSZ, MAXSZ, CV_8UC1 );
-//Tamano de la imagen que se creara
-//float mat[MAXSZ][MAXSZ];
+//Definicion del tamano de la imagen 
 int isPrime(int);
 int a[MAXSZ][MAXSZ],z;
-//thread function
+//Threads
 void *threadFunction(void *threadid){
    std::cout<<"thread called\n";
    long tid;
@@ -35,20 +32,18 @@ void *threadFunction(void *threadid){
    pthread_exit((void*) 0);
 }
 int main() {
-	//direccion inicial para que se mueva a la derecha
     int initial_direction = UP, n = MAXSZ, p = 1, num = MAXSZ * MAXSZ + 1;    
-	//rows y cols
     int r, c ;
+    //Donde r representa a rows de filas y c a columns de columnas 
     int row_right = 0, column_down = n - 1, row_left = n - 1, column_up = 0, colorVal = 0;
-    //MATRIZ a 0
     for (r = 0; r < MAXSZ; r++) {
         for (c = 0; c < MAXSZ; c++)
             a[r][c] = 0;
     }
-    //Generar espiral
+    //Generador de la espiral
+    //Movimiento en las 4 direcciones UP,RIGHT,DOWN y LEFT
     while (p != n * n + 1) {
         if (initial_direction == UP) {
-            //Mover a la derecha
             r = row_right++;
             for (c = 0; c < n; c++) {
                 if (a[r][c] == 0)
@@ -56,7 +51,6 @@ int main() {
             }
             initial_direction = RIGHT;
         } else if (initial_direction == RIGHT) {
-            //Mover hacia abajo
             c = column_down--;
             for (r = 0; r < n; r++) {
                 if (a[r][c] == 0)
@@ -64,7 +58,6 @@ int main() {
             }
             initial_direction = DOWN;
         } else if (initial_direction == DOWN) {
-            //Mover hacia la izqueirda
             r = row_left--;
             for (c = n - 1; c >= 0; c--) {
                 if (a[r][c] == 0)
@@ -72,7 +65,6 @@ int main() {
             }
             initial_direction = LEFT;
         } else if (initial_direction == LEFT) {
-            //Mover hacia arriba
             c = column_up++;
             for (r = n - 1; r >= 0; r--) {
                 if (a[r][c] == 0)
@@ -81,11 +73,11 @@ int main() {
             initial_direction = UP;
         }
     }
-    //creacion de threads
+    //Threads
     pthread_t threads[NUMTHREADS];
-    //Se define la variable que contendra el valor que retorne el thread
     void* return_status; 
 	int t; 
+	//Numero de threads 
     for (t = 1; t <= NUMTHREADS+1; t++){
         pthread_create(&threads[t], NULL, threadFunction, (void *) t);
     }
@@ -106,14 +98,13 @@ int main() {
 std::cout<<"Imagen guardada con exito como espiralPrimos.png";
     return 0;
 }
-//funcion dada para un numero primo
+//Numeros primos
 int isPrime(int number) {
     for (unsigned int i = 2; i < number; i++) {
         if (number % i == 0) {
             return 0;
         }
-    }
-    //number is prime	
+    }	
     return 255;
 }
 
